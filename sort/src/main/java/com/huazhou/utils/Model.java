c7aee983b50f4d64a4e6870d4083d03f90b14ef1
huazhou <huazhou@ucfgroup.com>
2015-11-12 19:23:20 +0800

    1

--- sort/src/main/java/com/huazhou/utils/Model.java
+++ sort/src/main/java/com/huazhou/utils/Model.java
@@ -2,33 +2,33 @@ package com.huazhou.utils;
 import com.huazhou.std.StdOut;
 
 /**
- * ÅÅĞòËã·¨Ä£°å
+ * æ’åºç®—æ³•ç±»æ¨¡æ¿
  */
 public class Model {
 	public void sort(Comparable[] a){
-		
+
 	}
-	
+
 	protected boolean less(Comparable v, Comparable w){
 		return v.compareTo(w) < 0;
 	}
-	
+
 	protected void exch(Comparable[] a, int i, int j){
 		Comparable t = a[i];
 		a[i] = a[j];
 		a[j] = t;
 	}
-	
+
 	public void show(Comparable[] a){
-		//ÔÚµ¥ĞĞÖĞ´òÓ¡Êı×é
+		//åœ¨å•è¡Œä¸­æ‰“å°æ•°ç»„
 		for (int i = 0; i < a.length; i++) {
 			StdOut.print(a[i] + " ");
 		}
 		StdOut.println();
 	}
-	
+
 	public boolean isSorted(Comparable[] a){
-		//²âÊÔÊı×éÔªËØÊÇ·ñÓĞĞò
+		//æµ‹è¯•æ•°ç»„å…ƒç´ æ˜¯å¦æœ‰åº
 		for (int i = 0; i < a.length; i++) {
 			if(less(a[i], a[i-1])){
 				return false;
