c686afb9312319fb56f5c59c3072ca416c2909fa
huazhou <huazhou@ucfgroup.com>
2015-11-14 18:52:50 +0800

    5

--- sort/src/main/java/Insertion.java
+++ sort/src/main/java/Insertion.java
@@ -11,7 +11,7 @@ public class Insertion extends Model{
 		System.out.println("Insertion");
 		int N = a.length;
 		//将a[i]插入到a[i-1]、a[i-2]、a[i-3]...之中
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i < N; i++) {
 			for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
 				exch(a, j, j-1);
 			}
